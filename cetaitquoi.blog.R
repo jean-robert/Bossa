bb4c7b27bc346c667fe8614a0249c780e332c7cc
Jean-Robert <jravettand@gmail.com>
2012-04-18 23:19:02 +0200

    changing commit comment format

--- cetaitquoi.blog.R
+++ cetaitquoi.blog.R
@@ -36,7 +36,7 @@ for(this.pair in new.u.pairs[!(new.u.pairs %in% old.u.pairs)]) {
   system(paste('mv', tmpf, paste('_posts/',filename,sep='')))
   system(paste('git add', paste('_posts/',filename,sep='')))
   system(paste("git commit -m 'adding a new song: ", gsub('[ [:punct:]]', ' ', nova.db[r, 'artiste']), '-',
-               gsub('[ [:punct:]]', ' ', nova.db[r,'titre']), "'", sep=''))nova.db[r, 'artiste'], " - ", nova.db[r, 'titre'], "'", sep=''))
+               gsub('[ [:punct:]]', ' ', nova.db[r,'titre']), "'", sep=''))
   system('git checkout master')
 
 }
